a548e08e91e61925ecf131d222d8877cabd25e41
Joseph Cava <bink.lynch@gmail.com>
2023-10-06 21:40:29 -0600

    Log url

--- index.mjs
+++ index.mjs
@@ -10,7 +10,7 @@ import {
 export const handler = async (event, context) => {
   console.log(event);
   console.log(context);
-  console.log("origin: " + event.headers.origin);
+  console.log("origin: " + new URL(event.headers.origin));
   try {
     switch (event.routeKey) {
       case "DELETE /v1/{proxy+}":
